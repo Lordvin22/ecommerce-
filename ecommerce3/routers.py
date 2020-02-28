4ba95eca94a1f356a696c0f58967a57188249fde
Dev-1 <dev-1@Dev-1s-MacBook-Air.local>
2020-02-28 10:19:10 -0600

    complete user on front y back

--- ecommerce3/routers.py
+++ ecommerce3/routers.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from rest_framework import routers
 from products.viewsets import Product2ViewSets\
     ,ProductList, GetProductId, GetProductName,PurchaseCreate,PurchaseList, \
-    UserCreate, ProductoDetalle, PurchaseDetail, UserType, CartCreate, Javi, GetUser
+    UserCreate, ProductoDetalle, PurchaseDetail, UserType, CartCreate, Javi, GetUser, \
+    CreateProduct
 
 
 urlpatterns = [
     url(r'^v1/productlist', Product2ViewSets, name='product_set'),
     url(r'^v1/products/$', ProductList.as_view(), name='product_list'),
+    url(r'^v1/add_products/$', CreateProduct.as_view(), name='product_add'),
     url(r'^v1/get_productid/(?P<id>.+)', GetProductId.as_view(), name='get_productid'),
     url(r'^v1/get_productdetail/(?P<pk>[0-9]+)/$', ProductoDetalle.as_view()),
     url(r'^v1/products/(?P<pk>.+)/purchase/', PurchaseCreate.as_view(), name='get_purchase'),
